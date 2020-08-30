cee6dfd883fa4d55d33c34c797c9559f768882b0
Aleksander <aleksanderbo10@gmail.com>
2020-08-30 17:35:55 +0200

    new commit

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import random
 
 
+
 def newPassword():
-
-    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z"]
+    alphabetUpper = [c.upper() for c in alphabet]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     newPass = []
+    defaultLength = 50
 
-    for l in alphabet:
-        randomLetter = alphabet[random.randint(0,25)]
+    while defaultLength > 0:
+        randomLetter = alphabet[random.randint(0, 25)]
+        randomNumber = numbers[random.randint(0, 9)]
+        randomUpperLetter = alphabetUpper[random.randint(0, 25)]
+
         newPass.append(randomLetter)
-        newPassLength = newPass[0:passLength]
+        newPass.append(randomUpperLetter)
+        newPass.append(randomNumber)
+
+        defaultLength -= 1
+
+    newPassLength = ("".join([str(i) for i in newPass]))
+    print(newPassLength[0:passlength])
+
+    def save_pass_to_file():
+        with open("passwords.txt", "w") as f:
+            f.write(newPassLength)
 
 
-    print("".join(newPassLength))
-
-
-passLength = (int(input("Choose password length: ")))
-
+passlength = (int(input("Enter password length: ")))
 newPassword()
-
